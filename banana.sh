1c26f0f8bcadcdf25086c435dd1080faaff1837f
Leoniels <leonardo.niels@gmail.com>
2018-06-10 12:02:59 +0200

    Ordenado y arreglado!

--- banana.sh
+++ banana.sh
@@ -4,7 +4,7 @@
 # 2-Generar n y fi(n) aleatorio. (fpga_square_tester.c)
 # 3-Prueba de rendimienro (descifrado.py con fichero de test)
 # 4-Cifrar (clave encrypt_key.c)
-# 5-En viar datos para descifrar (donde ocurre la magia de verdad descifrado.py)
+# 5-Enviar datos para descifrar (donde ocurre la magia de verdad descifrado.py)
 
 if [ $# = 1 ];
 then
@@ -12,27 +12,26 @@ then
     cypher_time=$1
     
     #Random n
-    ./fpga_square_tester
+    bin/fpga_square_tester
     
     file_test_origin='decrypt_test.txt'
     file_test_exit='test_result.txt'
-    ./descifrado.py $file_test_origin $file_test_exit
+    py/descifrado.py $file_test_origin $file_test_exit
     
     cat 'test_result.txt'
     echo ' Segundotes'
     
-    ./fpga_ratio_analyze
+    bin/fpga_ratio_analyze
     
     #Use time to get a good key
-    ./encrypt_key $cypher_time
+    bin/encrypt_key $cypher_time
     
     file_origin='key_encrypted.txt'
     file_exit='key_decrypted.txt'
-    ./descifrado.py $file_origin $file_exit
+    py/descifrado.py $file_origin $file_exit
     
     cat 'key_decrypted.txt'
     echo ' Segundotes'
-    cat 'show_results.txt'
     
 else
     echo '$0 takes exactly 1 argument "cypher_time" (seconds)'
